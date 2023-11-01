d08b5438d4f5ded69b4e24c28f9a52d1844ffe29
Shubham Chouksey <135697559+shubham-1809@users.noreply.github.com>
2023-11-01 14:56:41 +0530

    Update client.js

--- Chat-Web-Application/js/client.js
+++ Chat-Web-Application/js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000');
+const socket = io('https://chat-application-olive-two.vercel.app/');
 
 // Get DOM elements in respective js variables
 const form = document.getElementById('send-container');
